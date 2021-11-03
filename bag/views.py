36e1d19fb6f6486bca352798d796429a1c284bf5
ShyanM <shyanmuscat24@gmail.com>
2021-11-03 21:19:10 +0000

    added sizes to produce model and size selector box to product template

--- bag/views.py
+++ bag/views.py
@@ -7,6 +7,7 @@ def view_bag(request):
     """ A view that renders the bag contents page """
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
@@ -20,4 +21,3 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
-
